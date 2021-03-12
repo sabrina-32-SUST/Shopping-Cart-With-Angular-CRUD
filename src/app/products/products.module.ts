63ab1288e031147b211353f6d43a8b629b4c69fd
Sabrina <sanutaayvi@gmail.com>
2021-03-12 19:21:26 +0600

    creating  componenet

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
+import { AddProductsComponent } from './add-products/add-products.component';
+import { ViewProductsComponent } from './view-products/view-products.component';
+import { ViewAllProductsComponent } from './view-all-products/view-all-products.component';
+import { UpdateProductsComponent } from './update-products/update-products.component';
+import { DeleteProductsComponent } from './delete-products/delete-products.component';
+import { ViewAllProductsByDateComponent } from './view-all-products-by-date/view-all-products-by-date.component';
+import { ViewAllProductsByCategoryComponent } from './view-all-products-by-category/view-all-products-by-category.component';
 
 
 @NgModule({
-  declarations: [ProductsComponent],
+  declarations: [ProductsComponent, AddProductsComponent, ViewProductsComponent, ViewAllProductsComponent, UpdateProductsComponent, DeleteProductsComponent, ViewAllProductsByDateComponent, ViewAllProductsByCategoryComponent],
   imports: [
     CommonModule,
     ProductsRoutingModule
